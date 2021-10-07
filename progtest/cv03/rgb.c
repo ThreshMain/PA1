7977bbe196f042b554a4abeaba43dcedcd63bbdb
ThreshMain <martinkos007@gmail.com>
2021-10-07 02:47:16 +0200

    PT-CV03: Formatting and unnecessary declaration deleted

--- progtest/cv03/rgb.c
+++ progtest/cv03/rgb.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-int rgbConvert(){
-    int r=255,g,b;
+int rgbConvert() {
+    int r, g, b;
     char c;
     printf("Zadejte barvu v RGB formatu:\n");
-    if(scanf(" rgb ( %d , %d , %d %c",&r,&g,&b,&c)==4){
-        if(r<0||g<0||b<0||r>255||g>255||b>255||c!=')'){
+    if (scanf(" rgb ( %d , %d , %d %c", &r, &g, &b, &c) == 4) {
+        if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255 || c != ')') {
             return -1;
         }
-        printf("#%02X%02X%02X\n", r,g,b);
+        printf("#%02X%02X%02X\n", r, g, b);
         return 1;
     }
     return -1;
 }
-int main(){
-    if(rgbConvert()==1) return 0;
+
+int main() {
+    if (rgbConvert() == 1) return 0;
     printf("Nespravny vstup.\n");
     return 1;
 }
