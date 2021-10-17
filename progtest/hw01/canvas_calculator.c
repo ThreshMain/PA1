e1e0609ec9e6b68bf32739e88a6b2fe66e6c544e
ThreshMain <martinkos007@gmail.com>
2021-10-17 17:19:18 +0200

    PT-HW01: Added comments

--- progtest/hw01/canvas_calculator.c
+++ progtest/hw01/canvas_calculator.c
@@ -8,10 +8,22 @@ typedef struct {
     double height;
 } rectangle_t;
 
+/**
+ * Compares 2 doubles with accounting for double inaccuracy
+ * @param x double
+ * @param y double
+ * @return 0 or 1 based on if 2 numbers are the same
+ */
 int equals(double x, double y) {
     return fabs(x - y) < (DBL_EPSILON * fabs(x + y));
 }
 
+/**
+ * Rounds up the number to the closes integer
+ * that is bigger or equal to the param
+ * @param number double
+ * @return rounded number
+ */
 int custom_round(double number) {
     int int_number = (int) number;
     if (equals(int_number, number)) {
@@ -20,14 +32,36 @@ int custom_round(double number) {
     return int_number + 1;
 }
 
+/**
+ * Compares 2 numbers with accounting for double inaccuracy
+ * and returns if first[number] is bigger then the second one[bigger_then].
+ * @param number double
+ * @param bigger_then double
+ * @return 1 if the number is bigger otherwise 0
+ */
 int bigger_then(double number, double bigger_then) {
     return number > bigger_then && !equals(number, bigger_then);
 }
 
-int smaller_or_equal(double number, double bigger_then) {
-    return number < bigger_then || equals(number, bigger_then);
+/**
+ * Compares 2 numbers with accounting for double inaccuracy
+ * and returns if first[number] is smaller or equal then the second one[smaller_then].
+ * @param number double
+ * @param smaller_then double
+ * @return 1 if the number is smaller or equal otherwise 0
+ */
+int smaller_or_equal(double number, double smaller_then) {
+    return number < smaller_then || equals(number, smaller_then);
 }
 
+/**
+ * Calculate the number of rectangles needed to cover the area of desired rectangle
+ * with accounting for the overlap of rectangles if needed.
+ * @param piece piece of rectangle to be user for covering the area
+ * @param desired rectangle size of desired area
+ * @param overlap how much should 2 pieces overlap while covering the area
+ * @return number of rectangles needed or -1 if overlap is too big
+ */
 int count_needed(rectangle_t piece, rectangle_t desired, double overlap) {
     int width = 1;
     int height = 1;
@@ -56,12 +90,24 @@ int count_needed(rectangle_t piece, rectangle_t desired, double overlap) {
     return 1;
 }
 
+/**
+ * Rotates rectangle by 90°
+ * this will change the original rectangle
+ * @param rectangle to be rotated
+ */
 void rotate_rectangle(rectangle_t *rectangle) {
     double tmp = rectangle->height;
     rectangle->height = rectangle->width;
     rectangle->width = tmp;
 }
 
+/**
+ * Calculates the minimal number of rectangles needed to cover an area by rotating rectangle
+ * @param piece rectangle will be rotated and changed
+ * @param desired rectangle area that should be covered
+ * @param overlap how much should 2 pieces overlap while covering the area
+ * @return number of rectangles needed or -1 if overlap is too big
+ */
 int minimal_count(rectangle_t *piece, rectangle_t *desired, double overlap) {
     int count = count_needed(*piece, *desired, overlap);
     rotate_rectangle(piece);
@@ -69,10 +115,21 @@ int minimal_count(rectangle_t *piece, rectangle_t *desired, double overlap) {
     return count < rotated_count && count > 0 ? count : rotated_count;
 }
 
+/**
+ * Verifies if rectangle dimensions are reasonable
+ * @param rectangle
+ * @return 1 if rectangle is valid otherwise 0
+ */
 int verify_rectangle(rectangle_t *rectangle) {
     return bigger_then(rectangle->height, 0) && bigger_then(rectangle->width, 0);
 }
 
+/**
+ * Loads rectangle by printing the message to console and scanning for user input separated by white space
+ * @param message to be printed to the user or null if non message should be shown
+ * @param result pointer to rectangle to which the result will be written to
+ * @return EXIT_SUCCESS if loaded successfully otherwise EXIT_FAILURE
+ */
 int load_rectangle(char *message,rectangle_t *result) {
     if (message != NULL) {
         printf("%s", message);
