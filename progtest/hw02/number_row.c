05daa8e2dbe31f47fd997015d80b0f927c5ae8f8
ThreshMain <martinkos007@gmail.com>
2021-10-24 17:35:47 +0200

    PT-HW02: Speed upgrade using smarter way to get the number

--- progtest/hw02/number_row.c
+++ progtest/hw02/number_row.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void number_of_digits(long number, int base, int *digits, long *compare) {
-    while (number >= *compare) {
-        *compare *= base;
-        *digits = *digits + 1;
-    }
-}
+typedef struct {
+    long number;
+    int index;
+    int digits;
+} result;
 
-void print_number(long number, int base) {
+void print_number(long number, int base, int digits_count) {
     if (number == 0) {
         putchar('0');
         return;
     }
-    int digits_count=1;
-    long compare = base;
-    number_of_digits(number, base, &digits_count, &compare);
     char *digits = (char *) malloc(sizeof(int) * digits_count);
     int index = 0;
     while (number > 0) {
@@ -23,10 +19,29 @@ void print_number(long number, int base) {
         digits[index++] = (char) (digit + (digit > 9 ? 'a' - 10 : '0'));
         number /= base;
     }
-    for (int g = index-1; g >= 0; g--) {
+    for (int g = index - 1; g >= 0; g--) {
         putchar(digits[g]);
     }
     free(digits);
+}
+
+result get_number(long index, int base) {
+    long current_index;
+    long power = base;
+    int digits = 1;
+    while (1) {
+        current_index = (digits++) * power;
+        if (current_index > index) {
+            digits--;
+            result result;
+            result.number = index / digits;
+            result.index = index % digits;
+            result.digits = digits;
+            return result;
+        }
+        index += power;
+        power *= base;
+    }
 }
 
 int main(void) {
@@ -35,23 +50,13 @@ int main(void) {
     printf("Pozice a soustava:\n");
     int err;
     while ((err = scanf("%ld %d", &position, &base)) == 2 && position >= 0 && base >= 2 && base <= 36) {
-        long number = 0;
-        int digits=1, current_position = 0;
-        long compare=base;
-        while (1) {
-            number_of_digits(number, base,&digits,&compare);
-            current_position += digits;
-            if (current_position > position) {
-                break;
-            }
-            ++number;
-        }
-        print_number(number, base);
+        result result = get_number(position, base);
+        print_number(result.number, base, result.digits);
         putchar('\n');
-        for (int i = 0; i < position - (current_position - digits); ++i) {
+        for (int i = 0; i < result.index; ++i) {
             putchar(' ');
         }
-        puts("^");
+        printf("%c\n", '^');
     }
     if (err != EOF) {
         printf("Nespravny vstup.\n");
