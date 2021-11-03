53e4c180d5e9d7b6db5459c35d14cc04c3f3f5db
ThreshMain <martinkos007@gmail.com>
2021-11-04 00:10:54 +0100

    PT-HW03: More test cases for edge cases

--- progtest/hw03/tester.c
+++ progtest/hw03/tester.c
@@ -7,8 +7,6 @@
 #include <limits.h>
 #include "energy_calculator.h"
 
-int x = 0;
-
 void assertEquals(long long int b, long long int a) {
     if (a != b) {
         printf("Expected %lld got %lld diff %lld", a, b, a - b);
@@ -50,7 +48,8 @@ int main(int argc, char *argv[]) {
     assertEquals(energyConsumption(2021, 1, 1, 13, 15, 2021, 10, 5, 11, 20, &consumption), 1);
     assertEquals(consumption, 81106033LL);
     printf("11\n");
-    assertEquals(energyConsumption(2024, 1, 1, 13, 15, 2024, 10, 5, 11, 20, &consumption), 1);
+    assertEquals(energyConsumption(2024, 1, 1, 13, 15,
+                                      2024, 10, 5, 11, 20, &consumption), 1);
     assertEquals(consumption, 81398919LL);
     printf("12\n");
     assertEquals(energyConsumption(1900, 1, 1, 13, 15, 1900, 10, 5, 11, 20, &consumption), 1);
@@ -63,14 +62,15 @@ int main(int argc, char *argv[]) {
                                    2074, 11, 15, 1, 0, &consumption), 1);
     assertEquals(consumption, 9153LL);
     printf("15\n");
-    assertEquals(energyConsumption(1904, 1, 4, 1, 22, 2166, 2, 15, 6, 16, &consumption), 1);
+    assertEquals(energyConsumption(1904, 1, 4, 1, 22,
+                                      2166, 2, 15, 6, 16, &consumption), 1);
     assertEquals(consumption, 28040086770LL);
     printf("16\n");
     assertEquals(energyConsumption(1968, 11, 7, 4, 33, 2072, 1, 6, 6, 53, &consumption), 1);
     assertEquals(consumption, 11035972950);
     printf("17\n");
     assertEquals(energyConsumption(1947, 11, 27, 21, 41,
-                                   2051, 1, 2, 2, 18, &consumption), 1);
+                                      2051, 1, 2, 2, 18, &consumption), 1);
     assertEquals(consumption, 11028971561);
     printf("18\n");
     assertEquals(energyConsumption(1910, 11, 1, 3, 9,
@@ -84,6 +84,26 @@ int main(int argc, char *argv[]) {
     assertEquals(energyConsumption(2000, 12, 31, 23, 59,
                                       2001, 1, 1, 0, 0, &consumption), 1);
     assertEquals(consumption, 211LL);
+    long long int result=211LL;
+    int changeCost[] = {4, 5, 2, 3, 3, 1, 5, 4, 1, 2};
+    for(int i=0;i<10;i++){
+        printf("%d\n",22+i);
+        assertEquals(energyConsumption(2000, 12, 31, 23, 59,
+                                       2001, 1, 1, i, 0, &consumption), 1);
+        assertEquals(consumption, result);
+        result+=12200+changeCost[i];
+    }
+    printf("32\n");
+    energyConsumption(INT_MAX, 3, 30, 11, 12, INT_MAX, 3, 31, 10, 11, &consumption);
+    assertEquals(consumption, 280477);
+    printf("33\n");
+    energyConsumption(2000, 3, 30, 11, 12,
+                      2147483647, 3, 31, 10, 11, &consumption);
+    assertEquals(consumption, 229725435358133901);
+    printf("34\n");
+    energyConsumption(2000, 3, 29, 4, 11,
+                      2000, 3, 30, 3, 11, &consumption);
+    assertEquals(consumption, 280683);
     return 0;
 }
 
