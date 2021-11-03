8dd543c2dde103beb26d4d7e723012cced64151b
ThreshMain <martinkos007@gmail.com>
2021-11-03 21:47:01 +0100

    PT-HW03: More test cases

--- progtest/hw03/tester.c
+++ progtest/hw03/tester.c
@@ -11,7 +11,7 @@ int x = 0;
 
 void assertEquals(long long int b, long long int a) {
     if (a != b) {
-        printf("Expected %lld got %lld diff %lld", a, b,a-b);
+        printf("Expected %lld got %lld diff %lld", a, b, a - b);
     } else {
         printf("SUCCESS");
     }
@@ -44,7 +44,7 @@ int main(int argc, char *argv[]) {
     assertEquals(consumption, 67116LL);
     printf("9\n");
     assertEquals(energyConsumption(2021, 10, 1, 13, 15,
-                                      2021, 10, 2, 11, 20, &consumption), 1);
+                                   2021, 10, 2, 11, 20, &consumption), 1);
     assertEquals(consumption, 269497LL);
     printf("10\n");
     assertEquals(energyConsumption(2021, 1, 1, 13, 15, 2021, 10, 5, 11, 20, &consumption), 1);
@@ -60,26 +60,30 @@ int main(int argc, char *argv[]) {
     assertEquals(consumption, 146443LL);
     printf("14\n");
     assertEquals(energyConsumption(2074, 11, 15, 0, 15,
-                                      2074, 11, 15, 1, 0, &consumption), 1);
+                                   2074, 11, 15, 1, 0, &consumption), 1);
     assertEquals(consumption, 9153LL);
     printf("15\n");
     assertEquals(energyConsumption(1904, 1, 4, 1, 22, 2166, 2, 15, 6, 16, &consumption), 1);
     assertEquals(consumption, 28040086770LL);
     printf("16\n");
-    assertEquals(energyConsumption ( 1968, 11, 7, 4, 33, 2072, 1, 6, 6, 53, &consumption ),1);
+    assertEquals(energyConsumption(1968, 11, 7, 4, 33, 2072, 1, 6, 6, 53, &consumption), 1);
     assertEquals(consumption, 11035972950);
     printf("17\n");
-    assertEquals(energyConsumption ( 1947, 11, 27, 21, 41,
-                                        2051,  1,  2,  2, 18, &consumption ),1);
-    assertEquals(consumption,11028971561);
+    assertEquals(energyConsumption(1947, 11, 27, 21, 41,
+                                   2051, 1, 2, 2, 18, &consumption), 1);
+    assertEquals(consumption, 11028971561);
     printf("18\n");
-    assertEquals(energyConsumption ( 1910, 11, 1, 3, 9,
-                                     1983, 2, 17, 6, 31, &consumption ),1);
-    assertEquals(consumption,7733988800);
+    assertEquals(energyConsumption(1910, 11, 1, 3, 9,
+                                   1983, 2, 17, 6, 31, &consumption), 1);
+    assertEquals(consumption, 7733988800);
     printf("19\n");
-    assertEquals(energyConsumption ( 2059, 7, 4, 0, 47,
-                                     2073, 1, 11, 0, 24, &consumption ),1);
-    assertEquals(consumption,1446852166);
+    assertEquals(energyConsumption(2059, 7, 4, 0, 47,
+                                   2073, 1, 11, 0, 24, &consumption), 1);
+    assertEquals(consumption, 1446852166);
+    printf("20\n");
+    assertEquals(energyConsumption(2000, 12, 31, 23, 59,
+                                      2001, 1, 1, 0, 0, &consumption), 1);
+    assertEquals(consumption, 211LL);
     return 0;
 }
 
